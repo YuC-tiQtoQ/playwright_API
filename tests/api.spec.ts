f1bd53aa095430f5f1f502bdfea5becd1da63db5
YuC <258043326+YuC-tiQtoQ@users.noreply.github.com>
2026-01-29 16:26:45 +0000

    Update api.spec.ts

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,2 +1,18 @@
 import { test, expect } from '@playwright/test';
+// assert: order number is returned using using get/orders
+test('GET /orders', async( {request} ) => {
+  // get orders with params from met office api doc
+  const response = await request.get('/orders', {
+    params: {
+      detail: 'MINIMAL',
+      dataSpec: '1.1.0'
+    },
+  });
 
+  // assert: returns a json with order number. Response is ok
+  expect(response.ok()).toBeTruthy();
+
+  // check response json
+  const body = await response.json();
+  expect(body).toHaveProperty('orderId')
+})
